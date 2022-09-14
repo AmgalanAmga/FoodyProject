0443193c1f9a12295fb872aad92ce2dc959d47c6
AmgalanAmga <amka.storm9812@gmail.com>
2022-09-14 13:10:01 +0800

    select section

--- src/components/ChoosingMeal.jsx
+++ src/components/ChoosingMeal.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import { useState } from "react";
 import { foodDatas } from "../data";
 import { FoodCart } from "./FoodCart";
 import ellipse from "../images/Ellipse 22.png";
 import forkKnife from "../images/forkKnife.png";
-import { Box, Typography, Grid } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Select,
+  MenuItem,
+  InputLabel,
+  Typography,
+  FormControl,
+} from "@mui/material";
 export const ChoosingMeal = () => {
+  const [categoryValue, setVategoryValue] = useState("");
+  const selectArray = [
+    {
+      category: "Ангилах",
+      options: [
+        "Бүгд",
+        "Цагаан хоол",
+        "Цавуулаггүй",
+        "Хөнгөн хоол",
+        "Хүнд хоол",
+      ],
+    },
+    { category: "Үнэ", options: ["Бүгд", "Хямдаас үнэтэй", "Үнэтэйгээс хямд"] },
+  ];
+  const handleChange = (event) => {
+    setVategoryValue(event.target.value);
+  };
   return (
     <Box sx={{ mt: "100px", borderTop: "1px solid #C4C4C4" }}>
       <Box
@@ -34,14 +59,34 @@ export const ChoosingMeal = () => {
           Танд таалагдаж байгаа хоолоо сонгоно уу
         </Typography>
       </Box>
-      <Box sx={{ display: "flex", mt: 16 }}>
-        <Grid container sx={{ flex: 1 }} columnSpacing={4} rowSpacing={12}>
-          {foodDatas.map((food, i) => (
-            <Grid item xs={4}>
-              <FoodCart key={i} food={food} />
-            </Grid>
-          ))}
-        </Grid>
+      <Box sx={{ display: "flex", mt: 16, columnGap: 4 }}>
+        <Box sx={{ flex: 1 }}>
+          <Box sx={{ mb: 15, display: "flex", width: "100%", columnGap: 3 }}>
+            {selectArray.map((select, i) => (
+              <FormControl sx={{ flex: 1 }}>
+                <InputLabel>{select.category}</InputLabel>
+                <Select
+                  value={categoryValue}
+                  label={select.category}
+                  onChange={handleChange}
+                >
+                  {select.options.map((option, i) => (
+                    <MenuItem key={i} value={10}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            ))}
+          </Box>
+          <Grid container columnSpacing={4} rowSpacing={12}>
+            {foodDatas.map((food, i) => (
+              <Grid item xs={4} key={i}>
+                <FoodCart food={food} />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
         <Box sx={{ flex: 1 }}>2</Box>
       </Box>
     </Box>
