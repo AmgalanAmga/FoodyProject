0d130b007aeeca1978c2c529aa26934ca50a5475
AmgalanAmga <amka.storm9812@gmail.com>
2022-09-22 09:00:20 +0800

    firestore

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require("firebase-functions");
+const { user } = require("firebase-functions/v1/auth");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.newUserSignup = functions.auth.user().onCreate((user) => {
+  console.log(user.uid);
+  return admin.firestore().collection("users").doc(user.uid).set({
+    displayImage: user.photoURL,
+    email: user.email,
+    orders: [],
+  });
+});
---
+++ src/context/FirestoreContext.jsx
@@ -0,0 +1,3 @@
+import { useContext } from "react"
+import { firebaseConfig } from "../keys"
+import {   } from "firebase/firestore"
